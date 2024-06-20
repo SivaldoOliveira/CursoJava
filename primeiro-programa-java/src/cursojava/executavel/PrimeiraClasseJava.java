efb8b9df1a89300c9a11998fb5d6aa5577f0c028
Sivaldo Oliveira <oliveira.smo@outlook.com>
2024-06-19 21:16:48 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é o metodo auto executavel em java*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro progama em java");
+	}
+}
+
